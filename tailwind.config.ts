c4ceedd2743ddea2a0c7aa1696948974eb734a27
pongsakorn <pongsakorn@xspringdigital.com>
2024-07-06 13:04:43 +0700

    🏞️ add images, logos and homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-import { fontFamily } from "tailwindcss/defaultTheme";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config = {
   darkMode: ["class"],
